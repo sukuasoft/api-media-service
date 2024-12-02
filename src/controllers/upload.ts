8705eae4f2ba53bc1d6b9fce73fa12784001238d
Sebastião Sukuakueche <sukuakueches@gmail.com>
2024-12-02 20:00:39 +0100

    finish convert and upload
    Signed-off-by: Sebastião Sukuakueche <sukuakueches@gmail.com>

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -2,12 +2,18 @@ import { Request, Response } from "express";
 import { ApiResponse } from "../types/apiResponse";
 import { prisma } from "../prisma";
 import { v4 as uuidv4 } from "uuid";
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, writeFile, rename } from "fs/promises";
 import path from "path";
-import { pathMediaStorage } from "../utils/files";
+
+import {
+  getExtensionFile,
+  pathMediaStorage,
+  pathTempStorage,
+} from "../utils/files";
 import ffmpeg from "fluent-ffmpeg";
 import { internalError } from "../errors";
 import { validsMimeTypes } from "../utils/mimeTypes";
+import { TypeMedia } from "../types/enums/typeMediaEnum";
 
 async function POST(request: Request, response: Response<ApiResponse>) {
   const file = request.files!.file;
@@ -16,18 +22,20 @@ async function POST(request: Request, response: Response<ApiResponse>) {
     const id = uuidv4();
 
     //Definir as pastas
+    const rootTempDirFile = path.join(pathTempStorage, id);
     const rootDirFile = path.join(pathMediaStorage, id);
-    mkdir(rootDirFile);
-    const filePath = path.join(rootDirFile, file.name);
+    await mkdir(rootTempDirFile);
+
+    const filePathTemp = path.join(rootTempDirFile, file.name);
 
     //Criar as pastas
-    await writeFile(filePath, file.data);
+    await writeFile(filePathTemp, file.data);
 
     const metadados = await new Promise<{
       duration: number;
       size: number;
     } | null>((resolve) => {
-      ffmpeg.ffprobe(filePath, (err, metadata) => {
+      ffmpeg.ffprobe(filePathTemp, (err, metadata) => {
         if (err) {
           resolve(null);
         }
@@ -38,37 +46,124 @@ async function POST(request: Request, response: Response<ApiResponse>) {
       });
     });
 
-    if (!metadados){
-        internalError(response);
-        return;
+    if (!metadados) {
+      internalError(response);
+      return;
     }
 
     const mimeType = validsMimeTypes.get(file.mimetype);
 
-    if (mimeType){
-        const File = await prisma.file.create({
-            data: {
-                id: id, 
-                name: file.name, 
-                size: metadados.size, 
-                duration: metadados.duration, 
-                type: mimeType.type, 
-                mimetype: mimeType.name,
-                createdAt: new Date().toISOString()
-            }
-        });
-        
-        response.status(201).json({
-          success: true,
-          message: "File uploaded",
-          data: {
-            id: File.id, 
-            createdAt: File.createdAt.toISOString()
-          }
-        });
+    if (mimeType) {
+      //gerar thumbnail
+      if (mimeType.type == TypeMedia.VIDEO) {
+        if (
+          !(await new Promise((resolve) => {
+            ffmpeg(filePathTemp)
+              .thumbnail({
+                timestamps: [1],
+                filename: "thumbnail.jpg",
+                folder: rootTempDirFile,
+              })
+              .on("end", () => {
+                resolve(true);
+              })
+              .on("error", () => {
+                resolve(false);
+              });
+          }))
+        ) {
+          internalError(response);
+          return;
+        }
 
+
+        
+      }
+
+      else if (mimeType.type == TypeMedia.AUDIO){
+
+          let toExtension = '';
+          if (mimeType.extension == 'mp3') {
+            toExtension='wav';
+          }
+          else{
+            toExtension= 'mp3';
+          }
+
+          if (
+            !(await new Promise((resolve) => {
+              ffmpeg(filePathTemp).toFormat(toExtension)
+                .output(
+                  path.join(
+                    rootTempDirFile,
+                    `converted.${toExtension}`
+                  )
+                )
+                .on("end", () => {
+                  resolve(true);
+                })
+                .on("error", () => {
+    
+                  resolve(false);
+                }).run();
+            }))
+          ) {
+            internalError(response);
+            return;
+          }
+        
+      }
+
+
+      if (
+        !(await new Promise((resolve) => {
+          ffmpeg(filePathTemp)
+            .setStartTime("00:00:00")
+            .setDuration(metadados.duration > 30 ? 30 : metadados.duration)
+            .output(
+              path.join(
+                rootTempDirFile,
+                `short.${getExtensionFile(file.name)}`
+              )
+            )
+            .on("end", () => {
+              resolve(true);
+            })
+            .on("error", () => {
+
+              resolve(false);
+            }).run();
+        }))
+      ) {
+        internalError(response);
+        return;
+      }
+
+      //gravar na base de dados
+
+      const File = await prisma.file.create({
+        data: {
+          id: id,
+          name: file.name,
+          size: metadados.size,
+          duration: metadados.duration,
+          type: mimeType.type,
+          mimetype: mimeType.name,
+          createdAt: new Date().toISOString(),
+        },
+      });
+
+      await rename(rootTempDirFile, rootDirFile);
+
+      response.status(201).json({
+        success: true,
+        message: "File uploaded",
+        data: {
+          id: File.id,
+          createdAt: File.createdAt.toISOString(),
+        },
+      });
     }
-   
   }
 }
 
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 export const pathStorage = path.resolve(__dirname, '../../storage');
 export const pathMediaStorage = path.join(pathStorage, 'media');
+export const pathTempStorage = path.join(pathStorage, 'tmp');
 
 export function getExtensionFile(fileName: string): string {
   const splits = fileName.split(".");
